1e1cf5965031abdbe83d06f31e5f230a90cc9214
Ricardo Fernández Serrata <76864299+Rudxain@users.noreply.github.com>
2022-11-26 16:30:35 -0400

    Update lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -1,2 +1,24 @@
 #![deny(clippy::unwrap_used)]
 #![forbid(clippy::exit)]
+
+use std::fmt;
+
+pub enum GradientType {
+	Linear,
+	Radial,
+}
+
+impl fmt::Display for GradientType {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match self {
+            GradientType::Linear => write!(f, "linear"),
+            GradientType::Radial => write!(f, "radial"),
+        }
+    }
+}
+
+pub fn generate(t: GradientType, colors: Vec<&str>) -> &str {
+	let t = t.to_string();
+
+	return &colors.into_iter().map(|c| *c).collect::<Vec<&str>>().join("");
+}
